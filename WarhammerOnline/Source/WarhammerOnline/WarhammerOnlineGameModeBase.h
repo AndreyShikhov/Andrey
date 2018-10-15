2b9053adab543d1f2032d30024b40d1c4583c445
Andrey <Andrey.Shikhov94@yandex.ru>
2018-10-16 02:03:42 +0500

    add new projeckt

---
+++ WarhammerOnline/Source/WarhammerOnline/WarhammerOnlineGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarhammerOnlineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARHAMMERONLINE_API AWarhammerOnlineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ WarhammerOnline/Source/WarhammerOnlineServer.Target.cs
@@ -0,0 +1,15 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+[SupportedPlatforms(UnrealPlatformClass.Server)]
+public class WarhammerOnlineServerTarget : TargetRules
+{
+    public WarhammerOnlineServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		BuildEnvironment = TargetBuildEnvironment.Shared;
+		ExtraModuleNames.Add("WarhammerOnline");
+	}
+}
